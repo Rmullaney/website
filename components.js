f9ce1ee342399e7f154733b82ce927557695a031
Ronan Mullaney <rmullaney@colgate.edu>
2024-07-25 12:31:41 -0400

    testing custom nav-bar element

--- components.js
+++ components.js
@@ -8,9 +8,14 @@ class Navigation extends HTMLElement {
             <style>
                 nav {
                     background-color: darkred;
+                    width: 100vw;
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: space-evenly;
                 }
                 a {
                     background-color grey: 
+                    width: 30vw
                 }
             </style>
             <nav>
