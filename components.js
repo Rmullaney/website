87a7eef98da0475fb216ed8c647a6e1d5b406200
Ronan Mullaney <rmullaney@colgate.edu>
2024-11-05 17:59:29 -0500

    continuing linkedin img size debugging

--- components.js
+++ components.js
@@ -8,7 +8,7 @@ class Navigation extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            img {
+            .linkedin_img {
                 margin-left: 1vw;
                 width: 2vw;
                 height: auto;
@@ -19,8 +19,8 @@ class Navigation extends HTMLElement {
             <header class="sticky-top bg-primary">
                 <nav class="navbar navbar-expand-lg navbar-dark container">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="https://www.linkedin.com/in/ronanmullaney" target="_blank">
-                            <img src="images/linkedin.png" alt="Linkedin">
+                        <a href="https://www.linkedin.com/in/ronanmullaney" target="_blank">
+                            <img class="linkedin_img" src="images/linkedin.png" alt="Linkedin">
                         </a>
 
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
