ae16756ab7653ca911d349dff77a6c11928d33d3
Ronan Mullaney <rmullaney@colgate.edu>
2025-01-10 13:25:45 -0500

    tile style and value param

--- 2048-components/2048-tile.js
+++ 2048-components/2048-tile.js
@@ -1,13 +1,28 @@
 class Tile extends HTMLElement {
     constructor() {
         super();
+        this._value = 0;
+    }
+
+    // Specify which attributes to observe
+    static get observedAttributes() {
+        return ['value'];  // We'll observe the 'value' attribute
+    }
+
+    // This callback runs when the 'value' attribute is set or changed
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'value') {
+            this._value = parseInt(newValue, 10);  // Convert the attribute to a number
+            this.render();  // Re-render the element when the value changes
+        }
+    }
+
+    render() {
+        this.innerHTML = `<div class="tile">${this._value}</div>`;
     }
 
     connectedCallback() {
-        this.innerHTML = `
-            <div>craziest
-            </div>
-        `;
+        this.render()
     }
 }
 
