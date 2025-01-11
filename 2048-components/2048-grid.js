1b5b6b5bd0fab960afa8d2035ccad334a566b379
Ronan Mullaney <rmullaney@colgate.edu>
2025-01-11 17:15:15 -0500

    tiles given unique IDs

--- 2048-components/2048-grid.js
+++ 2048-components/2048-grid.js
@@ -9,37 +9,30 @@ class Grid extends HTMLElement {
         this.innerHTML = `
             <div class="d-flex flex-column">
                 <div class="d-flex flex-row">
-                    <tile-2048 value="2"></tile-2048>
-                    <tile-2048 value="4"></tile-2048>
-                    <tile-2048 value="8"></tile-2048>
-                    <tile-2048 value="16"></tile-2048>
+                    <tile-2048 id="tile-0" value="2"></tile-2048>
+                    <tile-2048 id="tile-1" value="4"></tile-2048>
+                    <tile-2048 id="tile-2" value="8"></tile-2048>
+                    <tile-2048 id="tile-3" value="16"></tile-2048>
                 </div>
                 <div class="d-flex flex-row">
-                    <tile-2048 value="2"></tile-2048>
-                    <tile-2048 value="4"></tile-2048>
-                    <tile-2048 value="8"></tile-2048>
-                    <tile-2048 value="16"></tile-2048>
+                    <tile-2048 id="tile-4" value="2"></tile-2048>
+                    <tile-2048 id="tile-5" value="4"></tile-2048>
+                    <tile-2048 id="tile-6" value="8"></tile-2048>
+                    <tile-2048 id="tile-7" value="16"></tile-2048>
                 </div>
                 <div class="d-flex flex-row">
-                    <tile-2048 value="2"></tile-2048>
-                    <tile-2048 value="4"></tile-2048>
-                    <tile-2048 value="8"></tile-2048>
-                    <tile-2048 value="16"></tile-2048>
+                    <tile-2048 id="tile-8" value="2"></tile-2048>
+                    <tile-2048 id="tile-9" value="4"></tile-2048>
+                    <tile-2048 id="tile-10" value="8"></tile-2048>
+                    <tile-2048 id="tile-11" value="16"></tile-2048>
                 </div>
                 <div class="d-flex flex-row">
-                    <tile-2048 value="2"></tile-2048>
-                    <tile-2048 value="4"></tile-2048>
-                    <tile-2048 value="8"></tile-2048>
-                    <tile-2048 value="16"></tile-2048>
+                    <tile-2048 id="tile-12" value="2"></tile-2048>
+                    <tile-2048 id="tile-13" value="4"></tile-2048>
+                    <tile-2048 id="tile-14" value="8"></tile-2048>
+                    <tile-2048 id="tile-15" value="16"></tile-2048>
                 </div>
             </div>
-
-
-
-
-           
-
-
         `;
     }
 }
